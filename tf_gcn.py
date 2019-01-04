755b3a8875294ab55b97187990f1e5eca01d3d7f
root <root@hyinghao@hotmail.com>
2019-01-04 10:55:46 +0800

    fixed errors

--- tf_gcn.py
+++ tf_gcn.py
@@ -3,6 +3,7 @@ import numpy as np
 from toy_dataset import ToyDataset
 from gcn_util import GCNUtil
 import random
+import math
 
 tds = ToyDataset()
 gcnu = GCNUtil()
@@ -32,37 +33,42 @@ for i in range(16):
     dsDegree = np.concatenate([dsDegree, D])
     dsFeatures = np.concatenate([dsFeatures, F])
 
-print("-"*20)
+print("-"*30)
 print("Shape information")
-print("-"*20)
+print("-"*30)
 print("adjacency", dsAdjacency.shape)
 print("features ", dsFeatures.shape)
 print("degree   ", dsDegree.shape)
 print("labels   ", dsLabels.shape)
-print("-"*20)
+print("-"*30)
 
 print()
 print()
 
+print("-"*30)
+print("Build model")
+print("-"*30)
 features = tf.placeholder(tf.float32, shape=(mSize,mSize))
 adjacency = tf.placeholder(tf.float32, shape=(mSize,mSize))
 degree = tf.placeholder(tf.float32, shape=(mSize,mSize))
 labels = tf.placeholder(tf.float32, shape=(6))
 
 
-weights = tf.Variable(tf.random_normal([mSize,mSize], stddev=1))
+weights_1 = tf.Variable(tf.random_normal([mSize,mSize], stddev=1))
+weights_2 = tf.Variable(tf.random_normal([mSize,mSize], stddev=1))
 
-def layer(features, adjacency, degree, weights):
+def GCN(features, adjacency, degree, weights):
     with tf.name_scope('gcn_layer'):
-        d_ = tf.pow(degree + tf.eye(mSize), -0.5)
+        d_ = tf.pow(degree + tf.eye(mSize), +1.0)
         y = tf.matmul(d_, tf.matmul(adjacency, d_))
         kernel = tf.matmul(features, weights)
 
         return tf.nn.relu(tf.matmul(y, kernel))
 
-m0 = layer(features, adjacency, degree, weights)
-model = tf.reshape(m0, shape=[-1, mSize, mSize, 1])
-model = tf.layers.conv2d(model, 1, kernel_size=2, activation=tf.nn.relu)
+gcn1 = GCN(features, adjacency, degree, weights_1)
+gcn2 = GCN(gcn1,     adjacency, degree, weights_2)
+
+model = tf.reshape(gcn2, shape=[-1, mSize, mSize])
 model = tf.contrib.layers.flatten(model)
 model = tf.layers.dense(model, 6)
 with tf.name_scope('loss'):
@@ -72,26 +78,33 @@ with tf.name_scope('loss'):
         )
     )
     train_op = tf.train.AdamOptimizer(0.0005, 0.9).minimize(loss)
-    
-print("-"*20)
+print()
+print()    
+print("-"*30)
 print("Training started")
-print("-"*20)
+print("-"*30)
 
-intializer = tf.initializers.global_variables()
+initializer = tf.initializers.global_variables()
 with tf.Session() as sess:
-    sess.run(intializer)
+    sess.run(initializer)
     
-    for i in range(500):
+    for i in range(200):
         rnd = random.randint(0,15)
         # remove improper classification category=2
         while np.argmax(dsLabels[rnd], axis=0) == 2 :
             rnd = random.randint(0,15)
-        _, lossVal, mVal = sess.run([train_op, loss, m0], feed_dict = {
+        _, lossVal = sess.run([train_op, loss], feed_dict = {
                 features: dsFeatures[rnd],
                 adjacency: dsAdjacency[rnd],
-                degree: dsDegree[rnd] + 0.05,
+                degree: dsDegree[rnd] + 0.001,
                 labels: dsLabels[rnd]
             }
         )
+        if lossVal == np.nan:
+            logLoss = np.nan
+        elif lossVal == 0.0:
+            logLoss = 0.0
+        else:
+            logLoss = math.log(lossVal)
         print("%d\t%2.6f"% (i, lossVal))
 
